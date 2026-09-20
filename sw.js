2a8150d57f0e06b1703660bec9b71deac2a6e943
cluj1313 <cluj1313@gmail.com>
2026-09-20 14:48:34 +0000

    V4: plan pe 5 stiluri și ecosistemul Ciubi în Improve
    Acordeon Supply/S&R/OB/FVG/MA, checklist editabil. Jurnalul vechi rămâne.

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'ciubi-v4-1';
+self.addEventListener('install', (e) => { self.skipWaiting(); });
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
