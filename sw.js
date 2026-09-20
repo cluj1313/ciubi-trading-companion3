6e30fe67451b662f44afac0e79c5e680b61d6858
cluj1313 <cluj1313@gmail.com>
2026-09-20 14:52:23 +0000

    Improve: poze mici pe carduri, nu bannere

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ciubi-v4-1';
+const CACHE = 'ciubi-v4-2';
 self.addEventListener('install', (e) => { self.skipWaiting(); });
 self.addEventListener('activate', (e) => {
   e.waitUntil(
